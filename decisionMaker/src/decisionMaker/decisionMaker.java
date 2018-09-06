3d0cfef69c015a54280ccecd5db2c0a5a3beabba
Ann <Ann@10.0.1.5>
2018-09-05 20:01:37 -0600

    worked on decision maker

--- decisionMaker/src/decisionMaker/decisionMaker.java
+++ decisionMaker/src/decisionMaker/decisionMaker.java
@@ -8,6 +8,17 @@ public class decisionMaker {
 	}
 	public static void makeDecision()
 	{
-		
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("What's your name?");
+		String userName = userInput.nextLine();
+		System.out.println("Alright " + userName + " welcome to the decision maker");
+		System.out.println("How many choices do you have to choose from?");
+		int numberOfChoices = userInput.nextInt();
+		String [] choices = new String[100]; 
+		for (int i = 0; i < numberOfChoices; i++)
+		{
+			System.out.print("next choice ?");
+			choices[i]  = userInput.nextLine();
+		}
 	}
 }
