8a3592a99b8e71380a18cbb7e2631162e3685a2f
Ann <Ann@10.0.1.5>
2018-09-05 19:04:48 -0600

    started decision maker

---
+++ decisionMaker/src/decisionMaker/decisionMaker.java
@@ -0,0 +1,13 @@
+package decisionMaker;
+import java.util.Scanner;
+public class decisionMaker {
+
+	public static void main(String[] args) 
+	{
+		makeDecision();
+	}
+	public static void makeDecision()
+	{
+		
+	}
+}
